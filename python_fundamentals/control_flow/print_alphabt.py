e9be77957b0de0c7f41138c7638ea8e05ba47175
aloladze88-afk <aloladze88@gmail.com>
2026-05-04 15:40:37 +0200

    remove extra newline from alphabets game

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 for letter in "abcdefghijklmnopqrstuvwxyz":
     if letter != "q" and letter != "e":
-        print("{}".format(letter), end="" if letter != "z" else "\n")
+        print("{}".format(letter), end="")
