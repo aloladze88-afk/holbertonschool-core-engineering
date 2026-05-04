d279eec02476a617f854a80b9d020660bf28868c
aloladze88-afk <aloladze88@gmail.com>
2026-05-04 15:35:42 +0200

    fix alphabet game

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
 for letter in "abcdefghijklmnopqrstuvwxyz":
     if letter != "q" and letter != "e":
-        print("{}".format(letter), end="")
-print()
+        print("{}".format(letter), end="" if letter != "z" else "\n")
+        
