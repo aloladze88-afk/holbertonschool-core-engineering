112a339aa796cf8410c86b7359d2a7573cd317f5
aloladze88-afk <aloladze88@gmail.com>
2026-05-04 13:46:44 +0200

    correct with pycodestyle

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -7,5 +7,5 @@ is_valid = version == 3
 
 print(f"Language: {language}")
 print(f"Version: {version}")
-print(f"Pi approx: {pi_value:.2f}")
+print(f"Pi approx: {pi_value: .2f}")
 print(f"Computation valid: {is_valid}")
