1b59ce0a68e79b6c7b3ec6e70df5f27b57734e2b
aloladze88-afk <aloladze88@gmail.com>
2026-05-08 15:39:49 +0200

    fix common elements

--- python_fundamentals/core_data_structures/common_elements.py
+++ python_fundamentals/core_data_structures/common_elements.py
@@ -1,4 +1,3 @@
-set_1 = {"Python", "C", "Javascript"}
-set_2 = {"Bash", "C", "Ruby", "Perl"}
-
-print(common_elements(set_1, set_2))
+#!/usr/bin/env python3
+def common_elements(set_1, set_2):
+    return set_1 & set_2
