af26760f2a54be9d586c125259a07bd1e1665ea4
aloladze88-afk <aloladze88@gmail.com>
2026-05-08 15:35:00 +0200

    correct print matrix integer

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-print_matrix_integer = __import__('print_matrix_integer').print_matrix_integer
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i != len(row) - 1:
+                print(" ", end="")
+        print()
