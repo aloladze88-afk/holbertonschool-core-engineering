f9b7dde1f1df997c31c642c9d234d6011e9f1424
aloladze88-afk <aloladze88@gmail.com>
2026-05-12 16:39:05 +0200

    add raise_exception.py

---
+++ python_fundamentals/exception_handling/raise_exception.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Define a function that deliberately raises a TypeError."""
+
+
+def raise_exception():
+    """Raise a TypeError when this function is called."""
+    raise TypeError
