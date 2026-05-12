34b8461f24e483b611064c7d3f476f1e6154b62c
aloladze88-afk <aloladze88@gmail.com>
2026-05-12 16:47:55 +0200

    finish raise_exception_msg

---
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Raise a NameError with a custom message."""
+
+
+def raise_exception_msg(message=""):
+    """Raise NameError using the given message."""
+    raise NameError
